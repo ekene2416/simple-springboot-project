f82ec8474da0769d66509b25717a258cb1ce998e
Ekeneuduike <112905408+Ekeneuduike@users.noreply.github.com>
2023-04-02 10:30:38 +0100

    simple project

---
+++ simple-spring-boot-project/src/main/java/com/ekene/simplespringbootproject/SimpleSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ekene.simplespringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootProjectApplication.class, args);
+	}
+
+}
